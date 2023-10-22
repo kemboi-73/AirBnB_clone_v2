a567d67461b77d8851e6b869de961b5dfaad7c3e
Mikey_254 <123319720+kemboi-73@users.noreply.github.com>
2023-10-22 22:48:20 +0300

    starts web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""Starts a simple Flask web application"""
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
+
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Routing to root(/), strict_slashes ensure the URL works with or without the '/'"""
-    return "Hello HBNB!"
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
-App.run(debug=True)
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host="0.0.0.0", port=5000, debug=None)
